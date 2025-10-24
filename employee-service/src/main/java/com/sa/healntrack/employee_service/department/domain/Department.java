185a97f05e2b09d6b467c12964c7dab118472d37
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-10-24 00:11:53 -0600

    feat: add department domain

--- employee-service/src/main/java/com/sa/healntrack/employee_service/department/domain/Department.java
+++ employee-service/src/main/java/com/sa/healntrack/employee_service/department/domain/Department.java
@@ -1,5 +1,58 @@
 package com.sa.healntrack.employee_service.department.domain;
 
+import java.util.Objects;
+
 public class Department {
-    
+
+    private final DepartmentCode code;
+    private final String name;
+    private final String description;
+    private boolean isActive;
+
+    public Department(DepartmentCode code,
+                      String name,
+                      String description) {
+        this.code = Objects.requireNonNull(code, "El código no puede ser nulo");
+        this.name = validateName(name);
+        this.description = normalizeDescription(description);
+        this.isActive = true;
+    }
+
+    public void deactivate() {
+        this.isActive = false;
+    }
+
+    public void activate() {
+        this.isActive = true;
+    }
+
+    private String validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("El nombre del departamento no puede ser vacío");
+        }
+        return name.trim();
+    }
+
+    private String normalizeDescription(String description) {
+        if (description == null || description.isBlank()) {
+            return null;
+        }
+        return description.trim();
+    }
+
+    public DepartmentCode getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
 }
--- employee-service/src/main/java/com/sa/healntrack/employee_service/department/domain/DepartmentCode.java
+++ employee-service/src/main/java/com/sa/healntrack/employee_service/department/domain/DepartmentCode.java
@@ -1,5 +1,15 @@
 package com.sa.healntrack.employee_service.department.domain;
 
-public record DepartmentCode() {
-    
+import java.util.Objects;
+
+public record DepartmentCode(String value) {
+
+    public DepartmentCode {
+        Objects.requireNonNull(value, "El código del departamento no puede ser nulo");
+        if (!value.matches("^[A-Z]{3}-\\d{3}$")) {
+            throw new IllegalArgumentException(
+                "El código del departamento debe cumplir el formato XXX-000. Ejemplo: COC-025"
+            );
+        }
+    }
 }
