8a8b385b54d37998eec0d85a33de6b4a4cdebac0
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-10-23 20:15:38 -0600

    chore: add project structure

---
+++ employee/src/test/java/com/sa/healntrack/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.healntrack.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
