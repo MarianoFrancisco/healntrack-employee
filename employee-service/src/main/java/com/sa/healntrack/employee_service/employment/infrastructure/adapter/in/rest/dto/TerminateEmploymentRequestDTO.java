69daf0d3ae67b72d742109541c6ba8a6223f7fc9
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-11-05 03:19:38 -0600

    fix: validations on terminate employment request dto

--- employee-service/src/main/java/com/sa/healntrack/employee_service/employment/infrastructure/adapter/in/rest/dto/TerminateEmploymentRequestDTO.java
+++ employee-service/src/main/java/com/sa/healntrack/employee_service/employment/infrastructure/adapter/in/rest/dto/TerminateEmploymentRequestDTO.java
@@ -4,8 +4,12 @@ import java.time.LocalDate;
 
 import com.sa.healntrack.employee_service.employment.domain.PeriodType;
 
+import jakarta.validation.constraints.NotNull;
+
 public record TerminateEmploymentRequestDTO(
+    @NotNull(message = "La fecha de terminación es obligatoria")
     LocalDate date,
+    @NotNull(message = "El tipo de terminación es obligatorio")
     PeriodType terminationType,
     String reason
 ) {
